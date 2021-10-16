c81fad097201da40fcc26f39c98c7f7b4e475633
Martin Oglesby <oglesby.mo@gmail.com>
2021-10-16 20:10:21 +0000

    Added comments throughout app.py Made new item button a modal menu

--- README.md
+++ README.md
@@ -5,17 +5,18 @@ Create a stock management website that makes use of a database to track how much
 # Database Plan
 
 stock:
--   item id
 -   name
--   price
--   quantity available
+-   type
+-   cost
+-   description
+-   image_url
+-   boughtBy
+-   sold
 users:
 -   Name
--   Items previously purchased
--   current basket
--   current balance
-orders:
--   
+-   password-hash
+-   balance
+
 
 stored on mongoDB - https://cloud.mongodb.com/v2/61139e25f0eba81f9f45682c#metrics/replicaSet/613f6895e52339380b7ad052/explorer/Stock/stock/find
 
@@ -77,12 +78,6 @@ Account has a button for previous purchases, as well as just listing user detail
 # Client Story Testing
 
 
-# Lighthouse: 
-
-
-# Mobile window sizes
-
-
 # Deployment
 
 
@@ -92,3 +87,40 @@ Account has a button for previous purchases, as well as just listing user detail
 
 
 # Credits: 
+
+LO1 Design, develop and implement a back-end for a full-stack web application using Python and a micro-framework
+
+- 1.1	Design a front end for a data-driven web application that meets accessibility guidelines, follows the principles of UX design, meets its given purpose and provides a set of user interactions
+- 1.2	Implement custom HTML and CSS code to create a responsive full-stack application consisting of one or more HTML pages with relevant responses to user actions and a set of data manipulation functions
+- 1.3	Build a non-relational database-backed Flask web application that allows users to store and manipulate data records about a particular domain.
+- 1.4	Design a database structure that is relevant for your domain, consisting of a minimum of one collection.
+- 1.5	Design and implement manual test procedures to assess functionality, usability, responsiveness and data management within the Full Stack web application
+- 1.6	Write Python code that is consistent in style and conforms to the PEP8 style guide (or another explicitly mentioned style guide, such as Google's) and validated HTML and CSS code.
+- 1.7	Include sufficient custom Python logic to to demonstrate your proficiency in the language
+- 1.8	Include functions with compound statements such as if conditions and/or loops in your Python code
+- 1.9	Write code that meets minimum standards for readability (comments, indentation, consistent and meaningful naming conventions).
+- 1.10	Name files consistently and descriptively, without spaces or capitalisation to allow for cross-platform compatibility.
+
+LO2 Demonstrate competence in modeling and managing non-relational data effectively
+
+- 2.1	Design a data model that fits the purpose of the project
+- 2.2	Develop the model into a usable non-relational database where data is stored in a consistent and well-organised manner.
+
+
+LO3 Demonstrate competence in querying and manipulating non-relational data effectively
+
+- 3.1	Create functionality for users to create, locate, display, edit and delete records
+
+
+LO4 Deploy a full stack web application to a cloud platform
+
+- 4.1	Deploy a final version of the full-stack application code to a cloud-based hosting platform (e.g. Heroku) and test to ensure it matches the development version
+- 4.2	Ensure that final deployed code is free of commented out code and has no broken internal links
+- 4.3	Document the deployment process in a README file in English that also explains the application’s purpose and the value that it provides to its users
+
+LO5 Identify and apply security features
+
+- 5.1	Use Git & GitHub for version control of a Full Stack web application up to deployment, using commit messages to document the development process.
+- 5.2	Commit final code that is free of any passwords or security sensitive information, to the repository and to the hosting platform
+- 5.3	Use environment variables, or files that are in .gitignore, to hide all secret keys
+- 5.4	Ensure that DEBUG mode is turned off in production versions
--- app.py
+++ app.py
@@ -27,37 +27,50 @@ def get_home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    #if POST request then attempt to register
     if request.method == "POST":
         username = request.form.get("username").lower()
+        #see if a user by same name already exists
         existing = mongo.db.users.find_one({"username": username})
         if existing:
+            #regect user creation if name already exists
             flash("user already exists")
             return redirect(url_for("register"))
         
+        #construct an entry in users database with hashed password
         register = {
             "username": username,
             "password": generate_password_hash(request.form.get("password")),
             "balance" : 0.0
         }
         mongo.db.users.insert_one(register)
+
+        #fix session cookies to have logged in user
         session["user"] = username
         session["basket"] = []
         flash("registration successful")
         return redirect(url_for("get_home"))
+
     return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    #if someone already logged in, log out and then go to login page
     if session.get("user"):
         logout()
 
+    #if POST then attempt to log in
     if request.method == "POST":
         username = request.form.get("username").lower()
         password = request.form.get("password")
 
+        #get user from users database with same username
         existing = mongo.db.users.find_one({"username": username})
+
+        #if entry doesnt exist or password has doesnt match, then redirect to login page again
         if existing and check_password_hash(existing["password"], password):
+            #log in session cookies set and tell user logged in
             session["user"] = username
             session["basket"] = []
             flash(f'logged in as {username}')
@@ -69,107 +82,117 @@ def login():
 
 @app.route("/logout")
 def logout():
+    #empty session and tell user its logged out
     session.clear()
     flash("Logged Out")
     return redirect(url_for("get_home"))
 
 @app.route("/user/<username>", methods=["GET", "POST"])
 def get_user(username):
-    if request.method == "POST":
+    #allows requesting arbritary user
+    if request.method == "POST": #if post request then user wanting to deposit money
         deposit_value = float(request.form.get("deposit"))
         update_balance(username, deposit_value)
         flash(f'£{deposit_value:.2f} deposited')
-        
+         
     user = mongo.db.users.find_one({"username": username})
-    if not user:
-        return redirect(url_for("get_user", username=session["user"]))
-    items = list(mongo.db.stock.find({"boughtBy": username}))
+    if not user: #if user requested doesnt exist, redirect to the logged in user
+        if session.get("user"):
+            return redirect(url_for("get_user", username=session["user"]))
+        return redirect(url_for("login")
+    #get every item of stock the user has bought
+    items = list(mongo.db.stock.find({"boughtBy": username, "sold": True}))
 
+    #saves if requested user is the logged in user or if the user is admin 
     logged_in = (username == session.get("user", "") or session.get("user","") == "admin")
-
     return render_template("user.html", user=user, logged_in=logged_in, items=items)
 
 @app.route("/deleteUser/<username>")
 def delete_account(username):
+    #only allow deleting if logged in user is admin or if they are the user trying to delete themself
     if username == session["user"] or session["user"] == "admin":
         mongo.db.users.delete_one({"username": username})
         flash("user deleted")
         session.clear()
-        return redirect(url_for("store"))
+        return redirect(url_for("get_home"))
     flash("hey you're not admin stop that")
-    return redirect(url_for("store"))
+    return redirect(url_for("get_home"))
 
 @app.route("/store", methods=["GET", "POST"])
 def store():
-    if request.method == "POST" and session.get("user", "") == "admin":        
-        item = request.form.to_dict()
-        item["sold"] = False
-        mongo.db.stock.insert_one(item)
-
-
     shop = list(mongo.db.stock.find())
-    for item in shop:
+    for item in shop: #create a comparable id entry in data since jinja sucks
         item["id"] = str(item["_id"])
-    #have edit button on each item change bottom box from a "new" item to a "edit" item, populated with old stuff
     return render_template("store.html", shop=shop)
 
 @app.route("/edit/<id>", methods=["GET", "POST"])
 def editItem(id):
-    print("hi")
-    if session.get("user", "")== "admin":
-        item = request.form.to_dict()
+    if session.get("user", "") == "admin": #only allow edit if admin
+        item = request.form.to_dict() #request form already follows correct format for data in database, so get that into dict
         item["sold"] = bool(item["boughtBy"])
-        b = mongo.db.stock.update_one({"_id": ObjectId(id)}, {"$set": item})
-        print(b)
+        mongo.db.stock.update_one({"_id": ObjectId(id)}, {"$set": item})
+    return redirect(url_for("store"))
+    
+@app.route("/newItem/", methods=["GET", "POST"])
+def newItem():
+    if session.get("user", "") == "admin": #only allow add if admin
+        item = request.form.to_dict() #request form already follows correct format for data in database, so get that into dict
+        item["sold"] = bool(item["boughtBy"])
+        mongo.db.stock.insert_one(item)
+    return redirect(url_for("store"))
+
+@app.route("/delete/<product_id>")
+def delete_item(product_id):
+    if session.get("user", "") == "admin": #only allow delete if admin
+        mongo.db.stock.remove({"_id": ObjectId(product_id)}) #delete item
+        flash("item removed from stock")
+        return redirect(url_for("store"))
     return redirect(url_for("store"))
 
 @app.route("/purchase/<product_id>")
 def add_basket(product_id):
     if product_id not in session["basket"]:
-        session["basket"].append(product_id)
-        session.modified = True
+        session["basket"].append(product_id) #add item to session cookies
+        session.modified = True #mark session as modified so that it saves edit
         flash("added to basket")
     return redirect(request.referrer)
 
 @app.route("/remove/<product_id>")
 def remove_basket(product_id):
     if product_id in session["basket"]:
-        session["basket"].remove(product_id)
-        session.modified = True
+        session["basket"].remove(product_id) #remove item from session cookies
+        session.modified = True #mark session as modified so that it saves edit
         flash("removed from basket")
     return redirect(request.referrer)
 
 
-@app.route("/delete/<product_id>")
-def delete_item(product_id):
-    if session.get("user", "") == "admin":
-        mongo.db.stock.remove({"_id": ObjectId(product_id)})
-        flash("item removed from stock")
-        return redirect(url_for("store"))
-    flash("hey you're not admin stop that")
-    return redirect(url_for("store"))
 
 @app.route("/cart")
 def getCart():
     #creates a query that returns every item in the sessions basket
     q = {"_id" : {"$in" : [ObjectId(id) for id in session["basket"]]}}
     basket = list(mongo.db.stock.find(q))
+
+    #sum item costs quickly
     cost = sum(float(item["cost"]) for item in basket)
     balance = get_balance(session["user"])
     return render_template("basket.html", basket=basket, totalCost=cost, balance=balance)
 
 @app.route("/sendPurchase")
-def purchaseAll():    
+def purchaseAll():
+    #creates a query that returns every item in the sessions basket
     q = {"_id": {"$in": [ObjectId(id) for id in session["basket"]]}}
     basket = list(mongo.db.stock.find(q))
+    #sum item costs quickly
     cost = sum(float(item["cost"]) for item in basket)
+    #only buy if can afford to
     if cost <= get_balance(session["user"]): 
+        #updates every item to be bought
         bought = {"$set": {"boughtBy": session["user"], "sold": True}}
         mongo.db.stock.update_many(q, bought)
         session["basket"] = []
         update_balance(session["user"], -cost)
         flash("items bought")
-
     
     return render_template("home.html")
 
